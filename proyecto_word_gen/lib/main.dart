68094122f5046eff22ecea747f06593fcaa658a4
davquev <ingdavquev@gmail.com>
2021-05-27 08:54:12 -0500

    Py words generator v2

--- proyecto_word_gen/lib/main.dart
+++ proyecto_word_gen/lib/main.dart
@@ -25,6 +25,7 @@ class RandomWordState extends State<RandomWord> {
   //final List<WordPair> suggestions = <WordPair>[];
   final suggestions = <WordPair>[];
   final TextStyle biggerFont = TextStyle(fontSize: 18);
+  final Set<WordPair> saved = Set<WordPair>();
 
   @override
   Widget build(BuildContext context) {
@@ -33,22 +34,28 @@ class RandomWordState extends State<RandomWord> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Generador de palabras!!!"),
+        actions: <Widget>[
+          IconButton(onPressed: pushSaved, icon: Icon(Icons.article_outlined))
+        ],
       ),
       body: buildSuggestions(),
     );
   }
 
-  Widget buildSuggestions(){
+  Widget buildSuggestions() {
     return ListView.builder(
       padding: EdgeInsets.all(12),
-      itemBuilder: (BuildContext context, int i){
-        if (i.isOdd){
-          print(i);
+      itemBuilder: (BuildContext context, int i) {
+        if (i.isOdd) {
+          //print(i);
           return Divider();
         }
 
         final int index = i ~/ 2;
-        if (index >= suggestions.length){
+        //print("i: ${i}");
+        //print("index: ${index}");
+        if (index >= suggestions.length) {
+          //print("se cumplio index: ${index}");
           suggestions.addAll(generateWordPairs().take(10));
         }
         return buildRow(suggestions[index]);
@@ -57,13 +64,56 @@ class RandomWordState extends State<RandomWord> {
   }
 
   Widget buildRow(WordPair pair) {
+    final bool alreadySaved = saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            saved.remove(pair);
+          }
+          else {
+            saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  void pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = saved.map(
+                  (WordPair pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: biggerFont,
+                  ),
+                );
+              }
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+              context: context,
+              tiles: tiles).toList();
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Favorites words"),
+            ),
+            body: ListView(
+              children: divided,
+            ),
+          );
+        },
+      ),
     );
   }
 }
-
-
